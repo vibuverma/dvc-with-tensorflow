fc599733b1e1d7a0111f5308b714255de350e653
Vaibhav Verma <vaibhavverma016@gmail.com>
2022-01-07 17:28:42 +0530

    tensorboard callback added

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -2,7 +2,13 @@ import yaml
 import os
 import json
 import logging
+import time
 
+
+def get_timestamp(name):
+    timestamp = time.asctime().replace(" ", "_").replace(":","_")
+    unique_name= f"{name}_at_{timestamp}"
+    return unique_name
 
 def read_yaml(path_to_yaml: str) -> dict:
     with open(path_to_yaml) as yaml_file:
--- src/utils/callbacks.py
+++ src/utils/callbacks.py
@@ -1,7 +1,21 @@
+import os
+import tensorflow as tf
+import time
+import joblib
+import logging
+from src.utils.all_utils import get_timestamp
 
 
 def create_and_save_tensorboard_callback(callbacks_dir, tensorboard_log_dir):
-    pass
+    unique_name= get_timestamp("tb_logs")
+
+    tb_running_log_dir= os.path.join(tensorboard_log_dir, unique_name)
+    tensorboard_callback= tf.keras.callbacks.TensorBoard(log_dir=tb_running_log_dir, histogram_freq=1)
+
+    tb_callback_filepath= os.path.join(callbacks_dir, "tensorboard_cb.cb")
+    joblib.dump(tensorboard_callback, tb_callback_filepath) # store the tf file in tb_callback_filepath
+
+    logging.info(f"tensorboard callback is saved at {tb_callback_filepath}")
 
 def create_and_save_checkpoint_callback(callbacks_dir, checkpoint_dir):
     pass
